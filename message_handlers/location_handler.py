cf7daa174563930399f4ddc395ab824563d3ff75
Pratyush More <pratyushmore96@gmail.com>
2018-08-22 23:09:31 +0800

    Changed message

--- message_handlers/location_handler.py
+++ message_handlers/location_handler.py
@@ -1,3 +1,3 @@
 def location(messaging_adaptor, user, channel, location):
-	message = "Your location has been set to {}. You are ready to be matched for Lunch Tag :)".format(location)
+	message = "Your location has been set to `{}`. You are ready to be matched for Lunch Tag :)".format(location)
 	messaging_adaptor.send_message(channel, message)
